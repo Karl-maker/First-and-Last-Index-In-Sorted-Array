b642fc42140430610b906930728dd5401670cfdf
Karl-Johan Bailey <73433921+Karl-maker@users.noreply.github.com>
2022-10-30 21:07:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var target = 5, output = [null, null], arr = [1, 2, 3, 5, 5, 5, 5, 8];
+var target = 5, output = [-1, -1], arr = [1, 2, 3, 5, 5, 5, 5, 8];
 
 for(i = 0; i < arr.length; i++)
 {
     if(target === arr[i]){
-        if(output[0] === null){
+        if(output[0] === -1){
             output[0] = i;
         } else {
             output[1] = i;
         }
+    } 
+    
+    if(output[0] !== -1 && output[1] === -1){
+        output[1] = output[0];
     }
 }
 
